22895a006cdb7437666653f588bf01dc77d0ee66
chertianser <serchertian@gmail.com>
2024-06-20 18:20:06 -0400

    AUROC 0.61

--- run_xgboost.py
+++ run_xgboost.py
@@ -7,7 +7,7 @@ import torch
 
 data = DataLoader()
 data.load_benchmark("leffingwell")
-data.featurize("morgan_fingerprints")
+data.featurize("rdkit2d_normalized_features")
 
 X = data.features
 y = data.labels
@@ -15,10 +15,9 @@ y = data.labels
 num_class = y.shape[1]
 
 X_train, y_train, X_test, y_test = iterative_train_test_split(X, y, test_size=0.1)
-bst = XGBClassifier(n_estimators=100000, max_depth=10000, learning_rate=0.1, 
-                    #num_class=num_class,
-                    objective='binary:logistic', early_stopping_rounds=10,
-                    eval_metric='logloss')
+bst = XGBClassifier(tree_method="hist",
+                        n_estimators=500, max_depth=10, learning_rate=0.1,
+                        early_stopping_rounds=10, eval_metric='logloss', n_jobs=-1)
 bst.fit(X_train, y_train,eval_set=[(X_test, y_test)])
 y_pred = bst.predict(X_test)
 
