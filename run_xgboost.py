d84589b56ea1b709f343e0ebba0d79f59cd91f0e
chertianser <serchertian@gmail.com>
2024-06-26 23:15:49 -0400

    80:20 split

--- run_xgboost.py
+++ run_xgboost.py
@@ -16,24 +16,23 @@ data.featurize("competition_rdkit2d")
 X = data.features.reshape(500, 1600)
 y = data.labels
 
-num_class = y.shape[1]
+X_train, y_train, X_test, y_test = iterative_train_test_split(X, y, test_size=0.2)
 
-X_train, y_train, X_test, y_test = iterative_train_test_split(X, y, test_size=0.1)
 if False:
     bst = XGBClassifier(tree_method="hist",
                             n_estimators=500, max_depth=10, learning_rate=0.1,
                             early_stopping_rounds=10, eval_metric='logloss', n_jobs=-1)
 else:
-    bst = XGBRegressor(n_estimators=5000, max_depth=100, learning_rate=0.1,
-                            early_stopping_rounds=10, eval_metric=mean_squared_error, n_jobs=-1)
+    bst = XGBRegressor(n_estimators=16000, max_depth=100, learning_rate=0.2,
+                            early_stopping_rounds=10, eval_metric=mean_squared_error, n_jobs=-1, random_state=42)
 
 bst.fit(X_train, y_train,eval_set=[(X_test, y_test)])
 y_pred = bst.predict(X_test)
 
-yt_pred = torch.from_numpy(y_pred)
-yt_test = torch.from_numpy(y_test)
 
 if False:
+    yt_pred = torch.from_numpy(y_pred)
+    yt_test = torch.from_numpy(y_test)
     auroc = multilabel_auroc(preds=yt_pred, target=yt_test, num_labels=y.shape[1])
     print(auroc)
 else:
